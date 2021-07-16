23d2a9189bbf481e7b7c42243ca56fb3d34b1eba
joshiawesome <dfelips@gmail.com>
2021-07-16 20:07:28 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators, FormGroupDirective, NgForm,} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'RegistrationForm';
+
+  countries=new FormControl();
+  country_list: string[]=['India','USA','Australia','England'];
+
+  states=new FormControl();
+  state_list: string[]=['Tamil Nadu','Uttar Pradesh','Karnataka','Kerela'];
+
+  gender_list: string[]=['male','female','other'];
+
+  firstFormGroup;
+  secondFormGroup;
+  matcher = new MyErrorStateMatcher();
+  
+  isLinear=true;
+  isEditable = false;
+
+  url="./assets/upload_logo.png"
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+
+    //validation for the first step
+    this.firstFormGroup = this._formBuilder.group({
+      name_validation: ['', Validators.required],
+      country_validation: ['', Validators.required],
+      state_validation: ['', Validators.required],
+      gender_validation:['',Validators.required],
+      phone_validation: new FormControl(undefined,[Validators.required])
+    });
+
+    //validation for the second step
+    this.secondFormGroup = this._formBuilder.group({
+      companyname_validation: ['', Validators.required],
+      email_validation: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      jobtitle_validation: ['', Validators.required],
+      experience_validation: ['', Validators.required],
+      agreement_validation: ['', Validators.required],
+    });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import{ MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NgxIntlTelInputModule } from "ngx-intl-tel-input";
+import {NgxMatIntlTelInputModule} from 'ngx-mat-intl-tel-input';
+import {NgOtpInputModule} from 'ng-otp-input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +21,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    NgxIntlTelInputModule,
+    NgxMatIntlTelInputModule,
+    NgOtpInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
